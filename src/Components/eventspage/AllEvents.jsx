adfd50f9ba424a5953f8e1c4a99f95122c2549bb
Rahul <chrahulofficial@gmail.com>
2023-11-11 14:31:21 +0530

    UI finished

--- src/Components/eventspage/AllEvents.jsx
+++ src/Components/eventspage/AllEvents.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { EventCard } from "./index";
 export default function Events() {
   return (
-    <div className="flex flex-col items-center justify-center gap-5 mx-auto my-8">
+    <main className="flex flex-col items-center justify-center gap-5 mx-auto my-8">
       <EventCard />
-   
-    </div>
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+    </main>
   );
 }
--- src/Components/eventspage/EventCard.jsx
+++ src/Components/eventspage/EventCard.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ProfilePic from "../ProfilePic";
 import { IoLocationOutline } from "react-icons/io5";
 import { AiOutlineHeart } from "react-icons/ai";
 import { PiShareFatFill } from "react-icons/pi";
-import { BsThreeDots } from "react-icons/bs";
+import { SlCalender } from "react-icons/sl";
 
 export default function EventCard() {
   return (
     <>
-      <div className="flex flex-col gap-5 shadow-xs shadow-gray-200 border-[0.1px] rounded-xl p-5 cursor-pointer">
+      <div className="flex flex-col gap-5  border-[0.1px] border-gray-300 rounded-xl p-5 cursor-pointer">
         <div className="space-y-2.5">
           <img
             src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=300"
@@ -19,6 +18,19 @@ export default function EventCard() {
             <h1 className="text-xl font-bold font-xl">Carnival Night Fest.</h1>
             <AiOutlineHeart size="22" color="gray" />
           </div>
+        </div>
+        <div className="flex items-start justify-between">
+          <div className="flex flex-col space-y-3 ">
+            <div className="flex items-center gap-x-3">
+              <SlCalender size="18" color="gray" />
+              <h1 className="text-xs text-slate-500">10 March 2024</h1>
+            </div>
+            <div className="flex items-center gap-x-3">
+              <IoLocationOutline size="18" color="gray" />
+              <h1 className="text-xs text-slate-500">Kompally Hyderabad</h1>
+            </div>
+          </div>
+          <PiShareFatFill size="18" color="gray" />
         </div>
       </div>
     </>
