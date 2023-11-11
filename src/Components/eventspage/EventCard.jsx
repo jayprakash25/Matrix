551d79e13fb6f41816d62ec1cd0ab4fabdce37ab
Rahul <chrahulofficial@gmail.com>
2023-11-11 21:41:11 +0530

    done the whole ui

--- src/Components/eventspage/EventCard.jsx
+++ src/Components/eventspage/EventCard.jsx
@@ -3,11 +3,18 @@ import { IoLocationOutline } from "react-icons/io5";
 import { AiOutlineHeart } from "react-icons/ai";
 import { PiShareFatFill } from "react-icons/pi";
 import { SlCalender } from "react-icons/sl";
+import { Link } from "react-router-dom";
 
 export default function EventCard() {
   return (
-    <>
-      <div className="flex flex-col gap-5  border-[0.1px] border-gray-300 rounded-xl p-5 cursor-pointer">
+    <Link to="/bookevents/Carnival Night Fest" state={{
+      image : "https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=300",
+      Tittle : "Carnival Night Fest",
+      date : "10 March 2024",
+      location:"Kompally Hyderabad",
+      about:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Et obcaecati, eligendi dolores aperiam ratione illum repellendus consequatur nisi non quas. necessitatibus incidunt! Odit necessitatibus accusantium quaerat commodi, animi minus praesentium porro quidem vitae eius ratione, enim impedit sit dolore molestiae consequunturnatus? Veritatis rerum commodi vel aut pariatur, animi omnis doloremque sunt ipsum reprehenderit possimus, vero assumenda expedita, dicta necessitatibus sit officiis? Fugiat, quis ipsam, tempora quos quasi mollitia ex quidem expedita illum facere non aspernatur sapiente fugit recusandae similique delectus. Quas incidunt ducimus deleniti eius expedita sunt quis non laboriosam saepe, suscipit illum aspernatur totam quam eveniet natus iure fuga. Assumenda dolor ipsa corporis asperiores. neque, harum quo enim error itaque molestiae consectetur ullam sint repellat quod quia, et eligendi voluptas quas quo blanditiis officiis totam nemo!"
+    }}>
+      <div className="flex flex-col gap-5 p-5 cursor-pointer rounded-xl">
         <div className="space-y-2.5">
           <img
             src="https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=300"
@@ -33,6 +40,6 @@ export default function EventCard() {
           <PiShareFatFill size="18" color="gray" />
         </div>
       </div>
-    </>
+    </Link>
   );
 }
--- src/Pages/BookEvents.jsx
+++ src/Pages/BookEvents.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { AiOutlineArrowLeftt } from "react-icons/ai";
+import { SlCalender } from "react-icons/sl";
+import { IoLocationOutline } from "react-icons/io5";
 
 export default function BookEvents() {
+  const event = useLocation();
+
+  const { image, Tittle, date, location, about } = event.state;
+
   return (
     <>
-      
+      <header>
+        <div className="absolute w-screen px-5 text-white top-5">
+          <div className="flex items-center gap-7">
+            <AiOutlineArrowLeft size={20} color={"white"} />
+            <h1 className="text-lg font-semibold">{Tittle}</h1>
+          </div>
+        </div>
+        <img
+          src={image}
+          className="w-[100vw]  h-[40vh] object-cover"
+          alt={image}
+        />
+      </header>
+      <main className="px-10">
+        <div className="mt-5 ">
+          <h1 className="max-w-sm text-3xl font-semibold leading-normal">
+            International Band Music Concert
+          </h1>
+          <div className="mt-8 space-y-7">
+            <div className="flex items-center gap-x-3">
+              <SlCalender
+                color="#6c7af7"
+                className="bg-[#eceefb]  w-12 h-12 p-3 rounded-full"
+              />
+              <div className="space-y-2">
+                <h1 className="text-xl font-semibold text-slate-700">{date}</h1>
+                <p>All are welcome.</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-x-3">
+              <IoLocationOutline
+                color="#6c7af7"
+                className="bg-[#eceefb]  w-12 h-12 p-3 rounded-full"
+              />
+              <div className="space-y-2">
+                <h1 className="text-xl font-semibold text-slate-700">
+                  {location}
+                </h1>
+                <p>Near the Sri chaitanya Collage Hyderabad</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-10 space-y-3">
+          <h1 className="text-2xl font-semibold text-slate-700">About Event</h1>
+          <p className="text-sm leading-6 text-gray-600">{about}</p>
+        </div>
+      </main>
+      <footer className="flex justify-center">
+        <div className="fixed bottom-5">
+          <button className="bg-[#5669fe] flex items-center text-center justify-center gap-x-5 font-semibold rounded-md text-white px-20 py-3.5 hover:brightness-75 ease-in-out duration-300">
+            <h1>Book your Ticket</h1>
+          </button>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
