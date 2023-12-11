6aa85f45a4a885a4d5d5911833b035ba582206b8
Rahul <chrahulofficial@gmail.com>
2023-12-11 23:08:01 +0530

    dn Notifications

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { BottomBar, Notification } from '../components'
+import React from "react";
+import { BottomBar, Notification } from "../components";
+import { LiaLessThanSolid } from "react-icons/lia";
+import { Link } from "react-router-dom";
 
 export default function Notifications() {
   return (
     <>
-       <Notification/>
-      <BottomBar/>
+      <nav className="p-4">
+        <div className="flex items-center w-[60vw] justify-between">
+          <div>
+            <Link to={"/home"}>
+              <LiaLessThanSolid size={20} color="black" />
+            </Link>
+          </div>
+
+          <div className="text-center">
+            <h1 className="text-lg font-semibold">Notifications</h1>
+          </div>
+        </div>
+      </nav>
+      <Notification />
+      <BottomBar />
     </>
-  )
+  );
 }
