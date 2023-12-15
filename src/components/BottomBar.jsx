409e9b69d5b8b4f526f2072f55dd217d570b18e4
unknown <jayprakashpj2525@gmail.com>
2023-12-15 20:09:10 +0530

    changed footer

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -9,29 +9,29 @@ export default function BottomBar() {
   const liststyle = "flex flex-col items-center gap-1 cursor-pointer";
 
   return (
-    <footer className="fixed bottom-0">
-      <div className="w-screen bg-black rounded-t-sm">
-        <ul className="flex items-center gap-5 p-3.5 text-sm font-semibold text-center text-white justify-evenly">
+    <footer className="fixed bottom-0 pb-5 px-5  w-full">
+      <div className="w-full bg-black rounded-full">
+        <ul className="flex items-center gap-5 p-3.5 px-2 text-sm font-semibold text-center text-white justify-evenly">
           <Link to={"/home"}>
             <li className={liststyle}>
               <PiHouse size={25} color="white" />
-              <h1>Home</h1>
+              {/* <h1>Home</h1> */}
             </li>
           </Link>
           <Link to={"/people"}>
             <li className={liststyle}>
               <IoMdPeople size={25} color="white" />
-              <h1>People</h1>
+              {/* <h1>People</h1> */}
             </li>
           </Link>
           <li className={liststyle}>
             <MdOutlineLocalPhone size={25} color="white" />
-            <h1>Contact</h1>
+            {/* <h1>Contact</h1> */}
           </li>
           <Link to={"/profile"}>
             <li className={liststyle}>
               <CgProfile size={25} color="white" />
-              <h1>Profile</h1>
+              {/* <h1>Profile</h1> */}
             </li>
           </Link>
         </ul>
