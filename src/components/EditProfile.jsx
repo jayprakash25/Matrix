82bce9b6d99b251d1567dd9e552e48af3c447923
unknown <jayprakashpj2525@gmail.com>
2023-12-21 15:37:41 +0530

    fetched

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -3,6 +3,54 @@ import { RxCross2 } from "react-icons/rx";
 import { IoCloudUploadOutline } from "react-icons/io5";
 
 export default function EditProfile({ setisedit }) {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       // const docRef = doc(db, "WHATWEDO", category);
+  //       const docRef = doc(db, "WHY-US", form.Category);
+
+  //       const docSnap = await getDoc(docRef);
+
+  //       if (docSnap.exists()) {
+  //         const data = docSnap.data();
+  //         setForm(data);
+  //       } else {
+  //         console.log("Document does not exist");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [form.Category]);
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     setIsSubmitting(true);
+
+  //     // Upload the image to Firebase Storage
+  //     const imageRef = ref(
+  //       storage,
+  //       `images/${form.Category}/${form.Image.name}`
+  //     );
+  //     await uploadBytesResumable(imageRef, form.Image);
+  //     const url = await getDownloadURL(imageRef);
+  //     const formData = {
+  //       ...form,
+  //       Image: url,
+  //     };
+  //     const docRef = doc(db, "WHY-US", form.Category);
+  //     await updateDoc(docRef, formData);
+  //     setIsSubmitting(false);
+  //     navigate(`/why-us`);
+  //   } catch (error) {
+  //     console.error("Error submitting data: ", error);
+  //     setIsSubmitting(false);
+  //   }
+  // };
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-75 backdrop-blur-md">
       <div className="p-6  bg-white  w-[90vw]">
