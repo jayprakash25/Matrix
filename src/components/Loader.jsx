e757f7cc0277d91912ecb6cfb0b36bb5e508bbb4
Rahul <chrahulofficial@gmail.com>
2023-12-24 22:46:28 +0530

    dn

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { LuLoader2 } from "react-icons/lu";
 
 export default function Loader() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-75 ">
+    <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-75 backdrop-blur-sm">
       <LuLoader2 size={30} color="black" className="animate-spin" />
     </div>
   );
