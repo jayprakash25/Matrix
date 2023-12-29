5c9c6a08128eb3eb166686bc6154e6783dc97a8c
Rahul <chrahulofficial@gmail.com>
2023-12-29 22:59:29 +0530

    dn

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,8 +2,8 @@ import { LuLoader2 } from "react-icons/lu";
 
 export default function Loader() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-75 backdrop-blur-sm">
-      <LuLoader2 size={30} color="black" className="animate-spin" />
+    <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-75 ">
+      <LuLoader2 size={30} color="orange" className="animate-spin" />
     </div>
   );
 }
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -3,9 +3,9 @@ import { CgProfile } from "react-icons/cg";
 import { MdOutlineLocalPhone } from "react-icons/md";
 import { PiHouse } from "react-icons/pi";
 import { ImExit } from "react-icons/im";
-import { AddPost, EditProfile, Loader } from "../components";
+import { AddPost, BottomBar, EditProfile, Loader } from "../components";
 import { Link } from "react-router-dom";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight, FaRegBell } from "react-icons/fa";
 import { FiEdit } from "react-icons/fi";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../Firebase";
@@ -45,130 +45,133 @@ export default function UserProfile() {
   }, []);
 
   return (
-    <main>
-      {isloading ? <Loader /> : null}
-      <nav className="p-4">
-        <div className="flex items-center w-[55vw] justify-between">
-          <div>
-            <Link to={"/home"}>
-              <FaArrowLeft size={20} color="black" />
-            </Link>
-          </div>
-
-          <div className="text-center">
-            <h1 className="text-lg font-semibold">My Profile</h1>
-          </div>
-        </div>
-      </nav>
-
-      <div className="flex items-start justify-center gap-5 mt-5">
-        <div className="">
-          <img
-            src={Userdata.Pic}
-            className="object-cover rounded-full w-36 h-36"
-            alt=""
-          />
-        </div>
-        <div className="max-w-[55vw] space-y-3">
-          <h1 className="text-lg font-bold">{Userdata.Name}</h1>
-          <p className="text-sm text-slate-500">{Userdata.Bio}</p>
-          <button
-            onClick={() => {
-              setisedit(true);
-            }}
-            className="py-2 text-sm font-semibold text-white bg-black rounded-lg px-7"
-          >
-            Edit Profile
-          </button>
-        </div>
-      </div>
-
-      <ul className="flex flex-col items-start justify-start gap-10 px-8 text-sm text-center py-7 my-">
-        <li className={"flex justify-between w-full items-center"}>
-          <Link to="/home">
-            <div className={liststyle}>
-              <PiHouse size={25} color="black" />
-              <h1>Home</h1>
+    <>
+      <main>
+        {isloading ? <Loader /> : null}
+        <nav className="p-4">
+          <div className="flex items-center w-[55vw] justify-between">
+            <div>
+              <Link to={"/home"}>
+                <FaArrowLeft size={20} color="orange" />
+              </Link>
             </div>
-          </Link>
-          <div>
-            <FaArrowRight size={20} color="black" />
-          </div>
-        </li>
-        <li className={"flex justify-between w-full items-center"}>
-          <div className={liststyle}>
-            <MdOutlineLocalPhone size={25} color="black" />
-            <h1>Contact</h1>
-          </div>
-          <div>
-            <FaArrowRight size={20} color="black" />
-          </div>
-        </li>
-        <li className={"flex justify-between w-full items-center"}>
-          <div
-            onClick={() => {
-              setisPost(true);
-            }}
-            className={liststyle}
-          >
-            <FiEdit size={25} color="black" />
-            <h1>Post</h1>
-          </div>
-          <div>
-            <FaArrowRight size={20} color="black" />
-          </div>
-        </li>
-        <li className={"flex justify-between w-full items-center"}>
-          <Link to="/privacy">
-            <div className={liststyle}>
-              <CgProfile size={25} color="black" />
-              <h1>Private & Policy</h1>
+
+            <div className="text-center">
+              <h1 className="text-lg font-semibold ">My Profile</h1>
             </div>
-          </Link>
-          <div>
-            <FaArrowRight size={20} color="black" />
           </div>
-        </li>
-        <li className={"flex justify-between w-full items-center"}>
-          <div className={liststyle}>
-            <ImExit size={25} color="" />
-            <h1 className="text-red-500">Logout</h1>
+        </nav>
+
+        <div className="flex items-start justify-center gap-5 mt-5">
+          <div className="">
+            <img
+              src={Userdata.Pic}
+              className="object-cover rounded-full w-36 h-36"
+              alt=""
+            />
           </div>
-          <div>
-            <FaArrowRight size={20} color="black" />
+          <div className="max-w-[55vw] space-y-3">
+            <h1 className="text-lg font-bold ">{Userdata.Name}</h1>
+            <p className="text-sm text-slate-500">{Userdata.Bio}</p>
+            <button
+              onClick={() => {
+                setisedit(true);
+              }}
+              className="py-2 text-sm font-semibold text-white rounded-lg bg-gradient-to-r from-yellow-500 via-amber-600 to-amber-700 px-7"
+            >
+              Edit Profile
+            </button>
           </div>
-        </li>
-      </ul>
-      <div className="flex flex-col items-center justify-center my-10 gap-7">
-        {Userdata.Posts?.map((i, index) => {
-          return (
-            <React.Fragment key={index}>
-              <div className="border-[1px] border-gray-200 rounded-lg shadow-sm max-w-md p-4 space-y-3.5">
-                <div className="flex items-center gap-5">
-                  <img
-                    src={i.Pic}
-                    className="object-cover w-12 h-12 rounded-full"
-                    alt=""
-                  />
-                  <h1 className="text-lg font-semibold text-slate-800">
-                    {i.Name}
-                  </h1>
-                </div>
-                <div>
-                  <img src={i.image} alt="" />
-                </div>
-                <div>
-                  <p className="text-sm font-semibold leading-6 text-slate-800">
-                    {i.Text}
-                  </p>
-                </div>
+        </div>
+
+        <ul className="flex flex-col items-start justify-start gap-10 px-8 text-sm text-center py-7 my-">
+          <li className={"flex justify-between w-full items-center"}>
+            <Link to="/home">
+              <div className={liststyle}>
+                <PiHouse size={25} color="orange" />
+                <h1>Home</h1>
               </div>
-            </React.Fragment>
-          );
-        })}
-      </div>
-      {isedit ? <EditProfile setisedit={setisedit} /> : null}
-      {isPost ? <AddPost setisPost={setisPost} /> : null}
-    </main>
+            </Link>
+            <div>
+              <FaArrowRight size={20} color="orange" />
+            </div>
+          </li>
+          <li className={"flex justify-between w-full items-center"}>
+            <div className={liststyle}>
+              <MdOutlineLocalPhone size={25} color="orange" />
+              <h1>Contact Us</h1>
+            </div>
+            <div>
+              <FaArrowRight size={20} color="orange" />
+            </div>
+          </li>
+          <li className={"flex justify-between w-full items-center"}>
+            <div
+              onClick={() => {
+                setisPost(true);
+              }}
+              className={liststyle}
+            >
+              <FaRegBell size={20} color="orange" />
+              <h1>Notifications</h1>
+            </div>
+            <div>
+              <FaArrowRight size={20} color="orange" />
+            </div>
+          </li>
+          <li className={"flex justify-between w-full items-center"}>
+            <Link to="/privacy">
+              <div className={liststyle}>
+                <CgProfile size={25} color="orange" />
+                <h1>Private & Policy</h1>
+              </div>
+            </Link>
+            <div>
+              <FaArrowRight size={20} color="orange" />
+            </div>
+          </li>
+          <li className={"flex justify-between w-full items-center"}>
+            <div className={liststyle}>
+              <ImExit size={25} color="orange" />
+              <h1 className="text-red-500">Logout</h1>
+            </div>
+            <div>
+              <FaArrowRight size={20} color="orange" />
+            </div>
+          </li>
+        </ul>
+        <div className="flex flex-col items-center justify-center my-10 gap-7">
+          {Userdata.Posts?.map((i, index) => {
+            return (
+              <React.Fragment key={index}>
+                <div className="border-[1px] border-gray-200 rounded-lg shadow-sm max-w-md p-4 space-y-3.5">
+                  <div className="flex items-center gap-5">
+                    <img
+                      src={i?.Pic}
+                      className="object-cover w-12 h-12 rounded-full"
+                      alt=""
+                    />
+                    <h1 className="text-lg font-semibold text-slate-800">
+                      {i?.Name}
+                    </h1>
+                  </div>
+                  <div>
+                    <img src={i?.image} alt={i?.image} />
+                  </div>
+                  <div>
+                    <p className="text-sm font-semibold leading-6 text-slate-800">
+                      {i?.Text}
+                    </p>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
+        </div>
+        {isedit ? <EditProfile setisedit={setisedit} /> : null}
+        {isPost ? <AddPost setisPost={setisPost} /> : null}
+      </main>
+      <BottomBar />
+    </>
   );
 }
