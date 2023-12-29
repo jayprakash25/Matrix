9a7b74049bec4277d21ad8757dec2cd5af735978
unknown <jayprakashpj2525@gmail.com>
2023-12-29 14:07:58 +0530

    cat

--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -1,4 +1,4 @@
-import { right, sports, tech } from "../../assets/home";
+import { sports, tech } from "../../assets/home";
 
 export default function Category() {
   const category = [
@@ -18,6 +18,22 @@ export default function Category() {
       icon: "",
       name: "Social",
     },
+    {
+      icon: "",
+      name: "Social",
+    },
+    {
+      icon: "",
+      name: "Social",
+    },
+    {
+      icon: "",
+      name: "Social",
+    },
+    {
+      icon: "",
+      name: "Social",
+    },
   ];
   return (
     <div className="px-6 py-[4.5rem] space-y-8">
@@ -25,22 +41,22 @@ export default function Category() {
         <h1 className="text-lg font-semibold">Categories</h1>
       </div>
       {/* <div className="flex items-center "> */}
-      <div className="grid grid-cols-4 relative">
+      <div className="flex space-x-4 relative overflow-x-auto">
         {category.map((_, index) => {
           return (
             <div key={index} className="grid  text-[0.75rem] space-y-3">
               <div className="bg-[#282828] w-16 h-16 rounded-3xl flex items-center justify-center">
                 <img className="w-7" src={_.icon} alt="" />
               </div>
-              <div className="place-items-center place-content-center grid pr-7">
+              <div className="place-items-center place-content-center grid ">
                 <p>{_.name}</p>
               </div>
             </div>
           );
         })}
-        <div className="flex items-center justify-center absolute right-0 top-[35%]  ">
+        {/* <div className="flex items-center justify-center absolute right-0 top-[35%]  ">
           <img src={right} alt="" />
-        </div>
+        </div> */}
       </div>
 
       {/* </div> */}
