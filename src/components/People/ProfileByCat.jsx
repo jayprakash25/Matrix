a6cf83d7405ec9e10e71a41f7ee8bf2d94a1e739
unknown <jayprakashpj2525@gmail.com>
2024-01-01 13:18:17 +0530

    category logic testying done

--- src/components/People/ProfileByCat.jsx
+++ src/components/People/ProfileByCat.jsx
@@ -1,17 +1,19 @@
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../Firebase";
 import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function ProfileByCat() {
-  const jwt = window.localStorage.getItem("jwt");
-  const [currentUser, setCurrentUser] = useState("");
+  // const jwt = window.localStorage.getItem("jwt");
+  const { category } = useParams();
+  // const [currentUser, setCurrentUser] = useState("");
   const [allUsers, setAllUsers] = useState([]);
 
   const fetchProfileByCat = useCallback(async () => {
     //getting current user details
-    const currentDocRef = doc(db, "USERS", jwt);
-    const currentUserSnapshot = await getDoc(currentDocRef);
-    const currentUser = currentUserSnapshot.data();
+    // const currentDocRef = doc(db, "USERS", jwt);
+    // const currentUserSnapshot = await getDoc(currentDocRef);
+    // const currentUser = currentUserSnapshot.data();
 
     //getting all users details
     const docRef = collection(db, "USERS");
@@ -24,20 +26,20 @@ export default function ProfileByCat() {
       }
     });
 
-    setCurrentUser(currentUser);
+    // setCurrentUser(currentUser);
     setAllUsers(usersData);
-  }, [jwt]);
+  }, []);
 
   //to filter users based on hobbies
-  const filterHobbies = async (currentUser, users) => {
+  const filterHobbies = async (category, users) => {
     return users.filter((user) => {
       // Assuming currentUser.hobbies is an array, check if user has any common hobbies
-      return user.hobbies.some((hobby) => currentUser.hobbies.includes(hobby));
+      return user.hobbies.some((hobby) => category === hobby);
     });
   };
 
   const fil = async () => {
-    const filteredUsers = await filterHobbies(currentUser, allUsers);
+    const filteredUsers = await filterHobbies(category, allUsers);
     console.log(filteredUsers);
   };
 
