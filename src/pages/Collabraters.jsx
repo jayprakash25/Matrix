0e6e188d4815514cdfa8b44291a1e45a5d6b3662
Rahul <chrahulofficial@gmail.com>
2024-01-15 13:43:39 +0530

    done

--- src/pages/Collabraters.jsx
+++ src/pages/Collabraters.jsx
@@ -10,45 +10,60 @@ export default function Collabraters() {
   const docref = doc(db, "USERS", jwt);
 
   // fetching User collabs list
-  const fetchUsercollabs = async () => {
+  const fetchUserCollabs = async () => {
     try {
-      const User = await getDoc(docref);
-      setcollabs(User.data().collabs || []);
+      const user = await getDoc(docref);
+      const currentConnectedUserCollabs = user.data()?.collabs || [];
+      const collabUsersData = await Promise.all(
+        currentConnectedUserCollabs.map(async (userId) => {
+          const userDocRef = doc(db, "USERS", userId);
+          const userDoc = await getDoc(userDocRef);
+          const { Name, Pic, Phone, Bio, location, Profession } =
+            userDoc?.data() || {};
+          return { userId, Name, Pic, Phone, Bio, location, Profession };
+        })
+      );
+      const collabsArray = collabUsersData;
+      console.log(collabsArray);
+      setcollabs(collabsArray);
       setisloading(false);
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      setisloading(false);
     }
   };
 
   useEffect(() => {
-    fetchUsercollabs();
+    fetchUserCollabs();
   }, []);
 
   return (
     <>
-      {isloading ? <NotifyLoader collabs={true} /> : null}
-      <section className="flex flex-col gap-5 mt-5 mb-20">
-        {collabs?.map((item, i) => {
-          return (
-            <React.Fragment key={i}>
-              <div className="flex items-center justify-around gap-10 rounded-lg border-[1px] mx-4 p-3 border-zinc-800 shadow-lg shadow-zinc-900">
-                <div className="flex items-start gap-5">
-                  <img
-                    className="object-cover rounded-full w-28 h-28"
-                    src={item.Pic}
-                    alt={item.Pic}
-                  />
-                  <div className="space-y-2.5">
-                    <h1 className="text-lg font-bold">{item.Name}</h1>
-                    <p className="text-xs leading-5">{item.Bio}</p>
+      {isloading ? (
+        <NotifyLoader collabs={true} />
+      ) : (
+        <section className="flex flex-col gap-5 mt-5 mb-20">
+          {collabs?.map((item, i) => {
+            return (
+              <React.Fragment key={i}>
+                <div className="flex items-center justify-around gap-10 rounded-lg border-[1px] mx-4 p-3 border-zinc-800 shadow-lg shadow-zinc-900">
+                  <div className="flex items-start gap-5">
+                    <div className="space-y-2.5">
+                      <h1 className="text-lg font-bold">{item.Name}</h1>
+                      <h1 className="font-semibold ">{item.Profession}</h1>
+                      <p className="font-semibold ">{item.Phone}</p>
+                      <p className="text-xs leading-6">{item.Bio}</p>
+                      <p className="text-xs leading-6">{item.location}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </React.Fragment>
-          );
-        })}
-        <BottomBar />
-      </section>
+              </React.Fragment>
+            );
+          })}
+        </section>
+      )}
+
+      <BottomBar />
     </>
   );
 }
