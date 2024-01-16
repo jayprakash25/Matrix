3e0c30d2781026b9a1432cf602280a9a08d1f2bb
unknown <jayprakashpj2525@gmail.com>
2024-01-16 11:27:55 +0530

    cat profile uis

--- src/components/People/ProfileByCat.jsx
+++ src/components/People/ProfileByCat.jsx
@@ -1,46 +1,13 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../Firebase";
-import { useCallback, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import BottomBar from "../BottomBar";
+import Profiles from "../ProfileCat.jsx/Profiles";
+import Discover from "../home/Discover";
 
 export default function ProfileByCat() {
-  const { category } = useParams();
-  const [allUsers, setAllUsers] = useState([]);
-  const [showusers, setshowusers] = useState([]);
-
-  const fetchProfileByCat = useCallback(async () => {
-    //getting all users details
-    const docRef = collection(db, "USERS");
-    const userSnapshot = await getDocs(docRef);
-    const usersData = [];
-    userSnapshot.forEach((doc) => {
-      const userData = doc?.data();
-      if (userData?.hobbies) {
-        usersData?.push(userData);
-      }
-    });
-    // setCurrentUser(currentUser);
-    setAllUsers(usersData);
-  }, []);
-
-  const filterHobbies = async (category, users) => {
-    return users?.filter((user) => {
-      return user?.hobbies?.some((hobby) => category === hobby);
-    });
-  };
-
-  const fil = async () => {
-    const filteredUsers = await filterHobbies(category, allUsers);
-    console.log(filteredUsers);
-    setshowusers(filteredUsers);
-  };
-
-  useEffect(() => {
-    fetchProfileByCat();
-  }, [fetchProfileByCat]);
-  useEffect(() => {
-    fil();
-  }, []);
-
-  return <div></div>;
+  return (
+    <div className="">
+      <Discover />
+      <Profiles />
+      <BottomBar />
+    </div>
+  );
 }
---
+++ src/components/ProfileCat.jsx/Profiles.jsx
@@ -0,0 +1,84 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../../Firebase";
+
+export default function Profiles() {
+  const { category } = useParams();
+  const [allUsers, setAllUsers] = useState([]);
+  const [showusers, setshowusers] = useState([]);
+
+  const fetchProfileByCat = useCallback(async () => {
+    //getting all users details
+    const docRef = collection(db, "USERS");
+    const userSnapshot = await getDocs(docRef);
+    const jwt = window.localStorage.getItem("jwt");
+    const usersData = [];
+    userSnapshot.forEach((doc) => {
+      const userData = doc?.data();
+      if (userData?.hobbies && jwt !== doc.id) {
+        usersData?.push(userData);
+      }
+    });
+    // setCurrentUser(currentUser);
+    setAllUsers(usersData);
+  }, []);
+
+  const filterHobbies = async (category, users) => {
+    return users?.filter((user) => {
+      return user?.hobbies?.some((hobby) => category === hobby);
+    });
+  };
+
+  const fil = useCallback(async () => {
+    const filteredUsers = await filterHobbies(category, allUsers);
+    setshowusers(filteredUsers);
+  }, [allUsers, category]);
+
+  console.log(showusers);
+
+  useEffect(() => {
+    fetchProfileByCat();
+  }, [fetchProfileByCat]);
+  useEffect(() => {
+    fil();
+  }, [fil]);
+  return (
+    <div className="p-5">
+      {showusers.map((_, index) => {
+        return (
+          <div
+            key={index}
+            className="w-full max-w-sm  border  rounded-lg shadow  border-zinc-800"
+          >
+            <div className="flex justify-between items-center ">
+              <div className="flex items-center space-x-2">
+                <img
+                  className="w-20 h-20 mb-3 rounded-full shadow-lg"
+                  src={_.Pic}
+                  alt="Bonnie image"
+                />
+                <div className="grid">
+                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                    {_.Name}
+                  </h5>
+                  <span className="text-sm text-gray-500 dark:text-gray-400">
+                    {_.Profession}
+                  </span>
+                </div>
+              </div>
+              <div className="flex">
+                <a
+                  href="#"
+                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Colloborate
+                </a>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
