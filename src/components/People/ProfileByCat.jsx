e712bc45ac37675f793b077ed54383d14f0c2d6c
Rahul <chrahulofficial@gmail.com>
2024-01-16 14:10:34 +0530

    done

--- src/components/People/ProfileByCat.jsx
+++ src/components/People/ProfileByCat.jsx
@@ -1,13 +1,40 @@
+import { useEffect } from "react";
 import BottomBar from "../BottomBar";
 import Profiles from "../ProfileCat.jsx/Profiles";
 import Discover from "../home/Discover";
+import { useAnimation, motion } from "framer-motion";
 
 export default function ProfileByCat() {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const startAnimation = async () => {
+      await controls.start("animate");
+    };
+    startAnimation();
+  }, [controls]);
+
+  const pageVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+  };
+
+  const pageTransition = { duration: 0.5 };
+
   return (
-    <div className="">
-      <Discover />
-      <Profiles />
+    <>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pageVariants}
+        transition={pageTransition}
+      >
+        <Discover />
+        <Profiles />
+      </motion.div>
       <BottomBar />
-    </div>
+    </>
   );
 }
--- src/components/ProfileCat.jsx/Profiles.jsx
+++ src/components/ProfileCat.jsx/Profiles.jsx
@@ -2,16 +2,17 @@ import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../Firebase";
+import NotifyLoader from "../notifications/NotifyLoader";
 
 export default function Profiles() {
   const { category } = useParams();
   const [allUsers, setAllUsers] = useState([]);
   const [showusers, setshowusers] = useState([]);
   const [connectedUser, setConnectedUser] = useState([]);
-  const jwt = window.localStorage.getItem("jwt");
+  const [isloading, setisloading] = useState(true);
+  const jwt = localStorage.getItem("jwt");
 
   const fetchProfileByCat = useCallback(async () => {
-    //getting all users details
     const docRef = collection(db, "USERS");
     const userSnapshot = await getDocs(docRef);
     const usersData = [];
@@ -24,8 +25,8 @@ export default function Profiles() {
         });
       }
     });
-    // setCurrentUser(currentUser);
     setAllUsers(usersData);
+    setisloading(false);
   }, [jwt]);
 
   const fetchCollabs = useCallback(async () => {
@@ -33,6 +34,7 @@ export default function Profiles() {
     const User = await getDoc(docRef);
     const collabs = User.data().collabs;
     setConnectedUser(collabs);
+    setisloading(false);
   }, [jwt]);
 
   const filterHobbies = async (category, users) => {
@@ -59,42 +61,46 @@ export default function Profiles() {
   console.log(connectedUser);
   return (
     <div className="p-5">
-      {showusers.map((_, index) => {
-        return (
-          <div
-            key={index}
-            className="w-full max-w-sm p-4 border  rounded-lg shadow  border-zinc-800"
-          >
-            <div className="flex justify-between items-center ">
-              <div className="flex items-center space-x-2">
-                <img
-                  className="w-20 h-20  rounded-full shadow-lg"
-                  src={_.Pic}
-                  alt="Bonnie image"
-                />
-                <div className="grid">
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    {_.Name}
-                  </h5>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    {_.Profession}
-                  </span>
+      {isloading ? (
+        <NotifyLoader />
+      ) : (
+        showusers.map((_, index) => {
+          return (
+            <div
+              key={index}
+              className="w-full p-4 border rounded-lg shadow max border-zinc-800"
+            >
+              <div className="flex items-center justify-between">
+                <div className="flex items-center space-x-5 ">
+                  <img
+                    className="w-20 h-20 rounded-full shadow-lg"
+                    src={_.Pic}
+                    alt="Bonnie image"
+                  />
+                  <div className="space-y-2">
+                    <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+                      {_.Name}
+                    </h5>
+                    <p className="text-sm font-semibold">{_.Profession}</p>
+                  </div>
+                </div>
+                <div className="flex">
+                  {connectedUser.some((user) => user === _.id) ? (
+                    <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300">
+                      Collaborated
+                    </button>
+                  ) : (
+                    <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      Colloborate
+                    </button>
+                  )}
+                  {console.log(_.id)}
                 </div>
               </div>
-              <div className="flex">
-                {connectedUser.some((user) => user === _.id) ? (
-                  <button>Coloborated</button>
-                ) : (
-                  <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Colloborate
-                  </button>
-                )}
-                {console.log(_.id)}
-              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 }
