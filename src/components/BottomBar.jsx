8cb633bf50b16c60d6f260c35d9323e3addd6680
unknown <jayprakashpj2525@gmail.com>
2024-01-18 12:30:13 +0530

    bottom bar

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -42,8 +42,8 @@ const BottomBar = () => {
   }, []);
 
   return (
-    <footer className="fixed bottom-0 w-full">
-      <div className="w-full bg-[#282828] rounded-t-xl">
+    <footer className="fixed bottom-0 w-full items-center justify-center flex">
+      <div className="w-[80vw] bg-[#282828] rounded-t-xl">
         <ul className="flex items-center px-2 py-2.5 text-sm font-semibold text-center text-white justify-evenly">
           <BottomBarItem
             to="/home"
