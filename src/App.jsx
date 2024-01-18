02934b9b42721bdce5ede0983b56f1a77ea6e24a
Rahul <chrahulofficial@gmail.com>
2024-01-18 22:26:29 +0530

    dn

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,6 @@ import ProfileByCat from "./components/People/ProfileByCat";
 export default function App() {
   const [isphone, setisphone] = useState(false);
 
-  const jwt = localStorage.getItem("jwt");
-
   useEffect(() => {
     const checkIsPhone = () => {
       setisphone(window.innerWidth < 1000);
@@ -49,25 +47,18 @@ export default function App() {
       {isphone ? (
         <>
           <Routes>
-            {jwt ? (
-              <>
-                <Route path="/hobbies" element={<SelectHobbies />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/profile" element={<UserProfile />} />
-                <Route path="/profile/:category" element={<ProfileByCat />} />
-                <Route path="/people/" element={<People />} />
-                <Route path="/notifications" element={<Notifications />} />
-                <Route path="/:userid" element={<ViewUserProfile />} />
-                <Route path="/privacy" element={<Privacy />} />
-                <Route path="/collabs/:id" element={<Collabraters />} />
-              </>
-            ) : (
-              <>
-                <Route path="/" element={<Signup />} />
-                <Route path="/register" element={<RegistrationForm />} />
-                <Route path="/login" element={<Login />} />
-              </>
-            )}
+            <Route path="/" element={<Signup />} />
+            <Route path="/register" element={<RegistrationForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/hobbies" element={<SelectHobbies />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/profile/:category" element={<ProfileByCat />} />
+            <Route path="/people/" element={<People />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/:userid" element={<ViewUserProfile />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/collabs/:id" element={<Collabraters />} />
           </Routes>
         </>
       ) : (
