b16c9e77d17e82bf864edf313917767b1b44b6a0
Rahul <chrahulofficial@gmail.com>
2024-01-26 02:22:13 +0530

    dn

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -31,18 +31,15 @@ export default function Works() {
         {works.map((item, i) => {
           return (
             <React.Fragment key={i}>
-              <div className="max-w-xs bg-bg-zinc-800 border-[1px] p-5 border-zinc-800">
+              <div className="border-[1px] p-5 border-zinc-800 ">
                 <div className="blur-sm  w-[50vw] h-[20vh] border-[1px] border-slate-900">
-                  <div className="absolute top-6 left-20">
+                  <div className="flex justify-center mt-5">
                     <CiLock size={70} />
                   </div>
                 </div>
                 <div className="space-y-3 text-center">
                   <h1 className="font-semibold">{item.Tittle}</h1>
                   <p className="text-[11px] leading-5">{item.Para}</p>
-                  <button className="py-1.5  text-center mt-3 text-[11px] font-bold text-white rounded-full bg-[#1d9bf0] px-5 w-[40vw] ">
-                    Collab
-                  </button>
                 </div>
               </div>
             </React.Fragment>
