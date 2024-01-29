e8e573be9e1b69c4e7238fc6746852ed53347ad1
Rahul <chrahulofficial@gmail.com>
2024-01-29 23:14:25 +0530

    dn

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -3,7 +3,7 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../Firebase";
 import Loader from "./Loader";
-
+import Emptyimg from "../images/Empty.png";
 export default function Notification() {
   const jwt = localStorage.getItem("jwt");
   const [isloading, setisloading] = useState(true);
@@ -52,35 +52,45 @@ export default function Notification() {
     <>
       {isloading ? <Loader /> : null}
       <main className="flex flex-col gap-4 mt-2">
-        {Notifications?.map((_, i) => {
-          return (
-            <React.Fragment key={i}>
-              <div className="flex items-center justify-around gap-10 rounded-lg border-[1px] mx-4 p-3 border-zinc-800 shadow-lg shadow-zinc-900">
-                <div className="flex items-center gap-5">
-                  <img
-                    className="object-cover w-20 h-20 rounded-full"
-                    src={_.Pic}
-                    alt={_.Pic}
-                  />
-                  <div className="space-y-2.5">
-                    <h1 className="text-lg font-bold">{_.Name}</h1>
-                    <p className="text-sm font-semibold">
-                      Want&apos;s to Collabrate with you
-                    </p>
+        {Notifications.length > 0 ? (
+          Notifications?.map((_, i) => {
+            return (
+              <React.Fragment key={i}>
+                <div className="flex items-center justify-around gap-10 rounded-lg border-[1px] mx-4 p-3 border-zinc-800 shadow-lg shadow-zinc-900">
+                  <div className="flex items-center gap-5">
+                    <img
+                      className="object-cover w-20 h-20 rounded-full"
+                      src={_.Pic}
+                      alt={_.Pic}
+                    />
+                    <div className="space-y-2.5">
+                      <h1 className="text-lg font-bold">{_.Name}</h1>
+                      <p className="text-sm font-semibold">
+                        Want&apos;s to Collabrate with you
+                      </p>
+                    </div>
                   </div>
+                  <AiOutlineDelete
+                    onClick={() => {
+                      DeleteNotification(i);
+                    }}
+                    size={27}
+                    cursor={"pointer"}
+                    color="white"
+                  />
                 </div>
-                <AiOutlineDelete
-                  onClick={() => {
-                    DeleteNotification(i);
-                  }}
-                  size={27}
-                  cursor={"pointer"}
-                  color="white"
-                />
-              </div>
-            </React.Fragment>
-          );
-        })}
+              </React.Fragment>
+            );
+          })
+        ) : (
+          <div className="flex flex-col items-center mt-1 space-y-3 text-cemt-11">
+            <img src={Emptyimg} alt="" className="w-60" />
+
+            <h1 className="text-sm font-semibold ">
+              You haven&apos;t posted anything yet!
+            </h1>
+          </div>
+        )}
       </main>
     </>
   );
--- src/components/notifications/Notification.jsx
+++ src/components/notifications/Notification.jsx
@@ -5,6 +5,7 @@ import NotifyLoader from "./NotifyLoader";
 import { RxCross2 } from "react-icons/rx";
 import { TiTickOutline } from "react-icons/ti";
 import { useNavigate } from "react-router-dom";
+import Emptyimg from "../../images/Empty.png";
 
 export default function Notification() {
   const jwt = localStorage.getItem("jwt");
@@ -72,76 +73,85 @@ export default function Notification() {
         <NotifyLoader />
       ) : (
         <main className="flex flex-col gap-4 mt-2">
-          {Notifications?.map((_, i) => {
-            return (
-              <React.Fragment key={i}>
-                <div className="flex items-center justify-around gap-2 rounded-lg border-[1px] mx-4 p-3 border-zinc-800 shadow-lg shadow-zinc-900">
-                  <div className="flex items-center gap-5">
-                    <img
-                      className="object-cover w-16 h-16 rounded-full"
-                      src={_.Pic}
-                      alt={_.Pic}
-                    />
-                    <div className="space-y-1">
-                      <h1 className="text-lg font-bold">{_.Name}</h1>
-                      <p className="text-sm font-semibold">
-                        Want&apos;s to Collabrate with you
-                      </p>
+          {Notifications.length > 0 ? (
+            Notifications?.map((_, i) => {
+              return (
+                <React.Fragment key={i}>
+                  <div className="flex items-center justify-around gap-2 rounded-lg border-[1px] mx-4 p-3 border-zinc-800 shadow-lg shadow-zinc-900">
+                    <div className="flex items-center gap-5">
+                      <img
+                        className="object-cover w-16 h-16 rounded-full"
+                        src={_.Pic}
+                        alt={_.Pic}
+                      />
+                      <div className="space-y-1">
+                        <h1 className="text-lg font-bold">{_.Name}</h1>
+                        <p className="text-sm font-semibold">
+                          Want&apos;s to Collabrate with you
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex gap-5">
+                      <RxCross2
+                        onClick={() => {
+                          DeleteNotification(i);
+                        }}
+                        size={25}
+                        cursor={"pointer"}
+                        color="red"
+                      />
+                      <TiTickOutline
+                        onClick={() => {
+                          setisshow(true);
+                        }}
+                        size={25}
+                        color="green"
+                      />
                     </div>
                   </div>
-                  <div className="flex gap-5">
-                    <RxCross2
-                      onClick={() => {
-                        DeleteNotification(i);
-                      }}
-                      size={25}
-                      cursor={"pointer"}
-                      color="red"
-                    />
-                    <TiTickOutline
-                      onClick={() => {
-                        setisshow(true);
-                      }}
-                      size={25}
-                      color="green"
-                    />
-                  </div>
-                </div>
-                {isshow ? (
-                  <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-25 backdrop-blur-md">
-                    <ul className="mx-5 space-y-4 rounded-md bg-zinc-900">
-                      <li className="cursor-pointer gap-7">
-                        <h1 className="p-4 ">
-                          By clicking 'Collab,' you agree to share your details
-                          with other users for the purpose of connecting and
-                          collaborating.
-                        </h1>
-                        <div className="border-b-[1px] border-zinc-700 w-full"></div>
-                        <div className="flex items-center justify-center">
-                          <div
-                            onClick={() => {
-                              setisshow(false);
-                            }}
-                            className="flex justify-center gap-2 px-4 pb-4 mt-3"
-                          >
-                            <h1 className="text-lg text-red-500">Cancel</h1>
+                  {isshow ? (
+                    <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-25 backdrop-blur-md">
+                      <ul className="mx-5 space-y-4 rounded-md bg-zinc-900">
+                        <li className="cursor-pointer gap-7">
+                          <h1 className="p-4 ">
+                            By clicking 'Collab,' you agree to share your
+                            details with other users for the purpose of
+                            connecting and collaborating.
+                          </h1>
+                          <div className="border-b-[1px] border-zinc-700 w-full"></div>
+                          <div className="flex items-center justify-center">
+                            <div
+                              onClick={() => {
+                                setisshow(false);
+                              }}
+                              className="flex justify-center gap-2 px-4 pb-4 mt-3"
+                            >
+                              <h1 className="text-lg text-red-500">Cancel</h1>
+                            </div>
+                            <div
+                              onClick={() => {
+                                acceptRequest(_.id);
+                              }}
+                              className="flex justify-center gap-2 px-4 pb-4 mt-3"
+                            >
+                              <h1 className="text-lg text-green-500">Collab</h1>
+                            </div>
                           </div>
-                          <div
-                            onClick={() => {
-                              acceptRequest(_.id);
-                            }}
-                            className="flex justify-center gap-2 px-4 pb-4 mt-3"
-                          >
-                            <h1 className="text-lg text-green-500">Collab</h1>
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                ) : null}
-              </React.Fragment>
-            );
-          })}
+                        </li>
+                      </ul>
+                    </div>
+                  ) : null}
+                </React.Fragment>
+              );
+            })
+          ) : (
+            <div className="flex flex-col items-center mt-24 space-y-3 text-cemt-11">
+              <img src={Emptyimg} alt="" className="w-60" />
+              <h1 className="text-sm font-semibold ">
+                You dont have any Notifications !
+              </h1>
+            </div>
+          )}
         </main>
       )}
     </>
