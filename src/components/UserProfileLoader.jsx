f0a6b00823c9e2f5f275fdb6aad3215c8fd37a6b
Rahul <chrahulofficial@gmail.com>
2024-02-16 11:01:58 +0530

    done

--- src/components/UserProfileLoader.jsx
+++ src/components/UserProfileLoader.jsx
@@ -4,7 +4,7 @@ function UserProfileLoader() {
       <div className="max-w-md px-4 py-3 rounded-lg shadow-sm lg border-[0.1px]  border-zinc-800">
         <div className="border-[0.1px] border-[rgb(33,32,32)] shadow rounded-md p-4  w-full mx-auto">
           <div className="flex flex-col animate-pulse">
-            <div className="mx-auto rounded-xl w-[70vw] h-[30vh] object-cover border border-[#282828] "></div>
+            <div className="mx-auto rounded-xl w-[70vw] h-[30vh] object-cover border-[1px] border-[#282828] "></div>
             <div className="flex-1 py-1 space-y-6">
               <div className="py-4 space-y-3">
                 <div className="grid grid-cols-3 gap-4">
