0db53ea0b47438d33a3c601f5c9cf2cc9d7cd07d
Rahul <chrahulofficial@gmail.com>
2024-02-17 15:01:32 +0530

    dn

--- src/components/notifications/Notification.jsx
+++ src/components/notifications/Notification.jsx
@@ -4,7 +4,6 @@ import { db } from "../../Firebase";
 import NotifyLoader from "./NotifyLoader";
 import { RxCross2 } from "react-icons/rx";
 import { TiTickOutline } from "react-icons/ti";
-import { useNavigate } from "react-router-dom";
 import Emptyimg from "../../images/Empty.png";
 
 export default function Notification() {
@@ -12,8 +11,6 @@ export default function Notification() {
   const [isloading, setisloading] = useState(true);
   const [Notifications, setNotifications] = useState();
   const [collabs, setcollabs] = useState([]);
-  const [isshow, setisshow] = useState(false);
-  const navigate = useNavigate();
 
   const getNotifications = async () => {
     try {
@@ -116,7 +113,7 @@ export default function Notification() {
                           />
                           <TiTickOutline
                             onClick={() => {
-                              setisshow(true);
+                              acceptRequest(_.id);
                             }}
                             size={25}
                             color="green"
@@ -134,38 +131,6 @@ export default function Notification() {
                       )}
                     </div>
                   </div>
-                  {isshow ? (
-                    <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-25 backdrop-blur-md">
-                      <ul className="mx-5 space-y-4 rounded-md bg-zinc-900">
-                        <li className="cursor-pointer gap-7">
-                          <h1 className="p-4 ">
-                            By clicking 'Collab,' you agree to share your
-                            details with other users for the purpose of
-                            connecting and collaborating.
-                          </h1>
-                          <div className="border-b-[1px] border-zinc-700 w-full"></div>
-                          <div className="flex items-center justify-center">
-                            <div
-                              onClick={() => {
-                                setisshow(false);
-                              }}
-                              className="flex justify-center gap-2 px-4 pb-4 mt-3"
-                            >
-                              <h1 className="text-lg text-red-500">Cancel</h1>
-                            </div>
-                            <div
-                              onClick={() => {
-                                acceptRequest(_.id);
-                              }}
-                              className="flex justify-center gap-2 px-4 pb-4 mt-3"
-                            >
-                              <h1 className="text-lg text-green-500">Collab</h1>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  ) : null}
                 </React.Fragment>
               );
             })
