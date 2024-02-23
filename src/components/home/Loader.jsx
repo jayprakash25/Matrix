1c56923ac9fff6e80d135566f3ef312d2779aec0
Jay <jayprakashpj2525@gmail.com>
2024-02-23 21:40:10 +0530

    profile ui

--- src/components/home/Loader.jsx
+++ src/components/home/Loader.jsx
@@ -1,14 +1,15 @@
+import React from "react";
 import PostLoader from "./PostLoader";
 
 export default function Loader() {
   return (
     <div className="pt-4">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => {
         return (
-          <>
+          <React.Fragment key={index}>
             <PostLoader />
             <PostLoader />
-          </>
+          </React.Fragment>
         );
       })}
     </div>
