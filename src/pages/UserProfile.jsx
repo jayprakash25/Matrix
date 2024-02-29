30fc42b6060e4552e6ce69f28d21216e1f14090c
Rahul <chrahulofficial@gmail.com>
2024-02-29 22:23:26 +0530

    dn

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -138,7 +138,7 @@ export default function UserProfile() {
               </div>
             </div>
           </nav>
-          <div className="flex items-start justify-center gap-2.5 mt-5 px-7">
+          <div className="flex items-start justify-center gap-2.5 mt-5 px-4">
             <div
               onClick={() => {
                 setEditImage(true);
@@ -153,7 +153,7 @@ export default function UserProfile() {
                       ? "https://firebasestorage.googleapis.com/v0/b/the-hub-97b71.appspot.com/o/6364b6fd26e2983209b93d18_ID_Playfal_DrawKit_Webflow_Display_2-min-png-934_2417--removebg-preview.png?alt=media&token=aa0f00e6-e1d5-4245-bfca-e5f6273ec980"
                       : currentUser.pic
                   }
-                  className="object-cover w-40 h-40 mx-auto border-[1px] border-gray-700 rounded-full"
+                  className="object-cover w-40 h-40 rounded-full"
                   alt={null}
                 />
                 {/* ) : ( */}
