b93ef1493f07b2985e46da87905447d23489210c
Rahul <chrahulofficial@gmail.com>
2024-03-31 21:17:32 +0530

    done

--- src/Data/Hobbies.jsx
+++ src/Data/Hobbies.jsx
@@ -1,80 +1,73 @@
-import GiteIcon from "@mui/icons-material/Gite";
-import HikingIcon from "@mui/icons-material/Hiking";
-import OutdoorGrillIcon from "@mui/icons-material/OutdoorGrill";
-import EditNoteIcon from "@mui/icons-material/EditNote";
-import PianoIcon from "@mui/icons-material/Piano";
-import PoolIcon from "@mui/icons-material/Pool";
-import TheatersIcon from "@mui/icons-material/Theaters";
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
-import MicIcon from "@mui/icons-material/Mic";
-import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
-import SportsTennisIcon from "@mui/icons-material/SportsTennis";
-import GamesIcon from "@mui/icons-material/Games";
-import HolidayVillageIcon from "@mui/icons-material/HolidayVillage";
-import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
-
+import AppsIcon from "@mui/icons-material/Apps";
+import SecurityUpdateWarningIcon from "@mui/icons-material/SecurityUpdateWarning";
+import PaymentIcon from "@mui/icons-material/Payment";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
+import FeaturedVideoIcon from "@mui/icons-material/FeaturedVideo";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import PaddingIcon from "@mui/icons-material/Padding";
+import BusinessIcon from "@mui/icons-material/Business";
+import LaptopIcon from "@mui/icons-material/Laptop";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
 const hobbies = [
   {
-    name: "Traveling ",
-    image: <GiteIcon color="primary" fontSize="small" />,
+    name: "AI",
+    image: <SmartToyIcon color="primary" fontSize="small" />,
   },
   {
     name: "Apps",
-    image: <PhotoCameraIcon color="primary" fontSize="small" />,
+    image: <AppsIcon color="primary" fontSize="small" />,
   },
   {
     name: "FinTech",
-    image: <OutdoorGrillIcon color="primary" fontSize="small" />,
+    image: <PaymentIcon color="primary" fontSize="small" />,
   },
   {
     name: "Cybersecurity",
-    image: <EditNoteIcon color="primary" fontSize="small" />,
+    image: <SecurityUpdateWarningIcon color="primary" fontSize="small" />,
   },
   {
     name: "E-learning",
-    image: <PoolIcon color="primary" fontSize="small" />,
+    image: <LocalLibraryIcon color="primary" fontSize="small" />,
   },
   {
     name: "Blockchain",
-    image: <MusicNoteIcon color="primary" fontSize="small" />,
+    image: <CurrencyBitcoinIcon color="primary" fontSize="small" />,
   },
   {
     name: "E-commerce",
-    image: <TheaterComedyIcon color="primary" fontSize="small" />,
+    image: <ShoppingCartIcon color="primary" fontSize="small" />,
   },
   {
     name: "Health ",
-    image: <HikingIcon color="primary" fontSize="small" />,
+    image: <HealthAndSafetyIcon color="primary" fontSize="small" />,
   },
   {
     name: "Content  ",
-    image: <PianoIcon color="primary" fontSize="small" />,
+    image: <DriveFileRenameOutlineIcon color="primary" fontSize="small" />,
   },
   {
     name: "Marketing",
-    image: <MicIcon color="primary" fontSize="small" />,
+    image: <FeaturedVideoIcon color="primary" fontSize="small" />,
   },
   {
     name: "Music",
-    image: <AutoFixHighIcon color="primary" fontSize="small" />,
+    image: <LibraryMusicIcon color="primary" fontSize="small" />,
   },
   {
     name: "Design",
-    image: <HolidayVillageIcon color="primary" fontSize="small" />,
+    image: <PaddingIcon color="primary" fontSize="small" />,
   },
   {
     name: "Real Estate",
-    image: <HolidayVillageIcon color="primary" fontSize="small" />,
+    image: <BusinessIcon color="primary" fontSize="small" />,
   },
   {
     name: "Agency",
-    image: <FitnessCenterIcon color="primary" fontSize="small" />,
-  },
-  {
-    name: "Supply-Chain",
-    image: <PhotoCameraIcon color="primary" fontSize="small" />,
+    image: <LaptopIcon color="primary" fontSize="small" />,
   },
 ];
 
